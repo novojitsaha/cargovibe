7e2a67ab7a34f904fd3ce1ca2158099cba44d956
Novojit Saha <n.saha@student.utwente.nl>
2025-09-22 13:13:08 +0200

    Setup nativewind

--- cargovibe_app/app/index.tsx
+++ cargovibe_app/app/index.tsx
@@ -2,12 +2,7 @@ import { Text, View } from "react-native";
 
 export default function Index() {
   return (
-    <View
-      style={{
-        flex: 1,
-        justifyContent: "center",
-        alignItems: "center",
-      }}
+    <View className="flex-1 justify-center items-center border border-red-600"
     >
       <Text>Edit app/index.tsx to edit this screen.</Text>
     </View>
