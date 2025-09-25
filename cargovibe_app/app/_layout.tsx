420c711257067a19bcfbbbc9d61c95e6630011f0
Novojit Saha <n.saha@student.utwente.nl>
2025-09-25 16:20:06 +0200

    Implement react-native-safe-area-context components

--- cargovibe_app/app/_layout.tsx
+++ cargovibe_app/app/_layout.tsx
@@ -1,5 +1,13 @@
 import { Stack } from "expo-router";
-import "./global.css"
+import "./global.css";
+import { SafeAreaProvider } from "react-native-safe-area-context";
 export default function RootLayout() {
-  return <Stack />;
+  return (
+    <SafeAreaProvider>
+      <Stack>
+        <Stack.Screen name="index" options={{headerShown:false}}/>
+        
+      </Stack>
+    </SafeAreaProvider>
+  );
 }
