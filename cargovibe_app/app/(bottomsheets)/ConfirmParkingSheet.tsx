54d318f28b5a14528a6ac87476809ac6a1d5b3a1
Novojit Saha <n.saha@student.utwente.nl>
2025-09-30 17:16:44 +0200

    Add ConfirmParkingSheet component with basic structure

---
+++ cargovibe_app/app/(bottomsheets)/ConfirmParkingSheet.tsx
@@ -0,0 +1,12 @@
+import React from "react";
+import { Text, View } from "react-native";
+
+const ConfirmParkingSheet = () => {
+  return (
+    <View>
+      <Text>ConfirmParkingSheet</Text>
+    </View>
+  );
+};
+
+export default ConfirmParkingSheet;
