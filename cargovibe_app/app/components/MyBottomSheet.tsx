c1a2bfacb9c8f6acbbe64b899214c4444b96bc41
Novojit Saha <n.saha@student.utwente.nl>
2025-10-01 14:55:17 +0200

    Add ConfirmParkingSheet handling to MyBottomSheet for parking spot selection

--- cargovibe_app/app/components/MyBottomSheet.tsx
+++ cargovibe_app/app/components/MyBottomSheet.tsx
@@ -1,15 +1,28 @@
 import { BottomSheetModal } from "@gorhom/bottom-sheet";
-import { useCallback, useEffect, useRef } from "react";
+import { useCallback, useEffect, useRef, useState } from "react";
 import { Keyboard } from "react-native";
+import ConfirmParkingSheet from "../(bottomsheets)/ConfirmParkingSheet";
 import ParkingListSheet from "../(bottomsheets)/ParkingListSheet";
 import SearchSheet from "../(bottomsheets)/SearchSheet";
+import ParkingSpotType from "../types/parkingSpot";
 export default function MyBottomSheet() {
   const searchSheetRef = useRef<BottomSheetModal>(null);
   const parkingListSheetRef = useRef<BottomSheetModal>(null);
+  const ConfirmParkingSheetRef = useRef<BottomSheetModal>(null);
+  const [parkingSpot, setParkingSpot] = useState<ParkingSpotType | undefined>(
+    undefined
+  );
 
   useEffect(() => {
     searchSheetRef.current?.present();
   }, []);
+
+  // Add this useEffect to open ConfirmParkingSheet when parkingSpot is set
+  useEffect(() => {
+    if (parkingSpot) {
+      openConfirmParkingSheet();
+    }
+  }, [parkingSpot]);
 
   const openParkingListSheet = useCallback(() => {
     Keyboard.dismiss(); // Dismiss the keyboard first
@@ -20,24 +33,28 @@ export default function MyBottomSheet() {
     parkingListSheetRef.current?.dismiss();
   }, []);
 
-  const handleParkingSpotPress = useCallback(() => {
+  const openConfirmParkingSheet = useCallback(() => {
+    ConfirmParkingSheetRef.current?.present();
+  }, []);
 
-    console.log("Parking spot pressed");
+  const closeConfirmParkingSheet = useCallback(() => {
+    ConfirmParkingSheetRef.current?.dismiss();
+    setParkingSpot(undefined); // Reset parking spot when closing
   }, []);
 
   return (
     <>
+      {/* Search Sheet Modal */}
       <BottomSheetModal
         ref={searchSheetRef}
         snapPoints={["25%", "60%"]}
         index={1}
-
         keyboardBehavior="extend"
         enablePanDownToClose={false}
       >
         <SearchSheet handleSearchButtonPress={openParkingListSheet} />
       </BottomSheetModal>
-
+      {/* Parking List Sheet Modal */}
       <BottomSheetModal
         ref={parkingListSheetRef}
         snapPoints={["85%"]}
@@ -46,10 +63,22 @@ export default function MyBottomSheet() {
         keyboardBehavior="extend"
         enablePanDownToClose={true}
       >
-        <ParkingListSheet 
+        <ParkingListSheet
           handleBackButtonPress={closeParkingListSheet}
-          handleParkingSpotPress={handleParkingSpotPress}
+          setParkingSpot={setParkingSpot}
         />
+      </BottomSheetModal>
+      {/* Confirm Parking Sheet Modal */}
+      <BottomSheetModal
+        ref={ConfirmParkingSheetRef}
+        snapPoints={["85%"]}
+        index={1}
+        stackBehavior="switch"
+        keyboardBehavior="extend"
+        enablePanDownToClose={true}
+      >
+        <ConfirmParkingSheet parkingSpot={parkingSpot}
+        handleBackButtonPress={closeConfirmParkingSheet} />
       </BottomSheetModal>
     </>
   );
